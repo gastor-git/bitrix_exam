3f8d31f1765f4fe79bb6550b95df4d4fb3df35b0
Tsybayev Artem <gastor.ru@gmail.com>
2019-07-21 15:32:10 +0300

    less9 ready

--- local/modules/tsybayev.exam/admin/menu.php
+++ local/modules/tsybayev.exam/admin/menu.php
@@ -3,28 +3,32 @@
 use Bitrix\Main\Localization\Loc;
 Loc::loadMessages(__FILE__);
 
-$menu = [
-    [
-        'parent_menu' => 'global_menu_services',
-        'section' => 'tsybayev_exam',
-        'sort' => 10,
-        'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE'),
-        'title' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE'),
-        "module_id" => "tsybayev_exam",
-        'items_id' => 'menu_tsybayev_exam',
-        "icon" => "util_menu_icon",
-        "page_icon" => "util_page_icon",
-        "items" => [
-            [
-                'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE_LIST'),
-                'url' => 'tsybayev_exam_admin_page_list.php?lang=' . LANGUAGE_ID,
-            ],
-            [
-                'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE_CREATE'),
-                'url' => 'tsybayev_exam_admin_page_create.php?lang=' . LANGUAGE_ID,
+if ($APPLICATION->GetGroupRight("tsybayev.exam") > "D") {
+    $menu = [
+        [
+            'parent_menu' => 'global_menu_services',
+            'section' => 'tsybayev_exam',
+            'sort' => 10,
+            'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE'),
+            'title' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE'),
+            "module_id" => "tsybayev_exam",
+            'items_id' => 'menu_tsybayev_exam',
+            "icon" => "util_menu_icon",
+            "page_icon" => "util_page_icon",
+            "items" => [
+                [
+                    'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE_LIST'),
+                    'url' => 'tsybayev_exam_admin_page_list.php?lang=' . LANGUAGE_ID,
+                ],
+                [
+                    'text' => Loc::getMessage('TSYBAYEV_EXAM_MENU_TITLE_CREATE'),
+                    'url' => 'tsybayev_exam_admin_page_create.php?lang=' . LANGUAGE_ID,
+                ],
             ],
         ],
-    ],
-];
+    ];
 
-return (!empty($menu)) ? $menu : [];
+    return $menu;
+}
+
+return [];
